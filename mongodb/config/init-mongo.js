e8fc80cb98afa2d2996be5775300ffb86ae609ec
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2021-08-04 18:30:59 +0400

    Added mongodb to environment

---
+++ mongodb/config/init-mongo.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "root",
+        pwd: "root$123",
+        roles: [
+            {
+                role: "readWrite",
+                db: "bisous"
+            }
+        ]
+    }
+)
